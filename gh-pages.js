eceecf4483e0d8a935e80764ccca02b7b096baae
deepak-parmar <dgparmar1999@rediffmail.com>
2022-03-24 17:26:34 +0530

    Update gh-pages

--- gh-pages.js
+++ gh-pages.js
@@ -1,16 +1,14 @@
 var ghpages = require('gh-pages');
 
 ghpages.publish(
-    'public', // path to public directory
-    {
-        branch: 'gh-pages',
-        repo: 'https://github.com/deepak-parmar/todo-svelte.git', // Update to point to your repository  
-        user: {
-            name: 'Deepak Parmar', // update to use your name
-            email: 'dgparmar1999@rediffmail.com' // Update to use your email
-        }
-    },
-    () => {
-        console.log('Deploy Complete!')
+  'public',
+  {
+    branch: 'gh-pages',
+    repo: 'https://github.com/deepak-parmar/todo-svelte.git', // Update to point to your repository  
+    user: {
+      name: 'Deepak Parmar',
+      email: 'dgparmar1999@rediffmail.com'
     }
+},
+  () => {console.log('Deploy Complete!')}
 )
